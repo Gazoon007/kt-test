b5e641a5556a67899914dc6f4de139881a85ac29
Alfarish Fizikri <alfarish@evermos.com>
2025-07-14 12:44:57 +0700

    feat(spinner): init loading spinner

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+// No props or logic needed for this simple component
+</script>
+
+<template>
+  <div class="flex justify-center items-center">
+    <div class="animate-spin rounded-full h-16 w-16 border-4 border-cyan-100 border-t-4 border-t-cyan-500 shadow-lg" />
+  </div>
+</template>
