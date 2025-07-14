2d471ade3501cbd16c273ec2fcaf75ad510c100f
Alfarish Fizikri <alfarish@evermos.com>
2025-07-14 12:58:44 +0700

    feat(user-store): add by page query key, fetch users, search users

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -5,6 +5,8 @@ import userService from '@/services/userService'
 // Define query keys similar to pinia-colada-demo approach by Pinia Creator
 export const USER_QUERY_KEYS = {
   root: ['users'],
+  byPage: (page: number, resultsPerPage: number) =>
+    USER_QUERY_KEYS.root.concat(['page', page.toString(), 'perPage', resultsPerPage.toString()]),
   search: (searchText: string, page: number, resultsPerPage: number) =>
     USER_QUERY_KEYS.root.concat(['search', searchText, 'page', page.toString(), 'perPage', resultsPerPage.toString()]),
 }
@@ -13,17 +15,22 @@ export const useUserStore = defineStore('user', () => {
   const seed = ref('kiratech')
   const sortOrder = ref('-')
   const currentUser = ref(null)
-  function setSortOrder(order: string) {
-    sortOrder.value = order
-  }
 
   return {
     USER_QUERY_KEYS,
     seed,
     sortOrder,
-    setSortOrder,
     currentUser,
 
+    setSortOrder(order: string) {
+      sortOrder.value = order
+    },
+    fetchUsers(page = 1, resultsPerPage = 10) {
+      return userService.fetchUsers(page, resultsPerPage, seed.value)
+    },
+    searchUsers(searchQuery: string, page = 1, resultsPerPage = 10) {
+      return userService.searchUsers(searchQuery, page, resultsPerPage, seed.value)
+    },
     clearCache() {
       userService.clearCache()
     },
